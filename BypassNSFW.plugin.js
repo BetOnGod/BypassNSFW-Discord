12c488f55101473703627d40032bc2648a137ba1
BetOnGod <148845871+BetOnGod@users.noreply.github.com>
2024-05-18 22:57:12 -0500

    Create BypassNSFW.plugin.js

---
+++ BypassNSFW.plugin.js
@@ -0,0 +1,37 @@
+/**
+     * @name BypassNSFW
+     * @author BetOnGod
+     * @description View Discord NSFW-Gated content without meeting the required age requirement.
+     * @version 1.0.0
+     * @source https://github.com/BetOnGod/BypassNSFW
+     * @updateUrl https://github.com/BetOnGod/BypassNSFW/raw/main/BypassNSFW.plugin.js
+*/
+    module.exports = class Bypass {
+
+        start() {
+
+            // temp 
+            setInterval(async () => {
+              await patch()
+
+            }, 5000);
+
+
+            async function patch() {
+                const UserStore = BdApi.findModuleByProps('getCurrentUser', 'getUser').getCurrentUser();
+                UserStore.nsfwAllowed = true;
+
+                if (UserStore.nsfwAllowed == false) {
+                    UserStore.nsfwAllowed = true;
+                }
+            }
+
+        }
+
+        stop() {
+            const UserStore = BdApi.findModuleByProps('getCurrentUser', 'getUser').getCurrentUser();
+            UserStore.nsfwAllowed = false;
+
+
+        };
+}
